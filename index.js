0756b16e67de3ed355144a7fed307d4d89dae551
VMR21 <reddymodith@gmail.com>
2025-06-19 08:28:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const SELF_URL = "https://mojotxdata.onrender.com/leaderboard/top14";
+const SELF_URL = "https://sasbthdata.onrender.com/leaderboard/top14";
 const API_KEY = "Pw0w9lsrb9iA69pVpK8aBrTOH0DymVOd";
 
 let cachedData = [];
